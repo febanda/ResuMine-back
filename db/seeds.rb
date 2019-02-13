fe357df97791fac51dce6db5653ff1f9d47497b2
febanda01 <febanda91@gmail.com>
2019-02-13 14:14:21 -0600

    initial

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-resume = Resume.create([{personal_info: {name: 'Stan', email: 'stan123@gmail.com', phone:'713-356-0987', title:'motivational speaker'}}{work_history: {company:'Exxon', years: '2010-2015', position:'CEO', responsibilites:'Mowing'}}{education: {}}])
+resume = Resume.create([{personal_info: {name: 'Stan', email: 'stan123@gmail.com', phone:'713-356-0987', title:'motivational speaker'}}{work_history: {company:'Exxon', years: '2010-2015', position:'CEO', responsibilites:'Mowing'}}{education: {school: ''}}])
